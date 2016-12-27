65b201188163422a412d82cac58caa48db4e56d4
Marcode777 <marcosebello7@gmail.com>
2016-12-27 11:48:57 -0500

    tested div implementation via styling and components, functional

--- src/components/display.js
+++ src/components/display.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+const Style1 = {
+  backgroundColor: "white",
+  minHeight: 500
+}
+
+const Style2 = {
+  backgroundColor: "gray",
+  minHeight: 300
+}
+
+
+
 export default class Display extends React.Component{
   render(){
     return(
-      <div>
-        <h2>DISPLAY FILE </h2>
+      <div className="container">
+        <div className="first" style={Style1}>
+          <h2>FIRST DISPLAY </h2>
+        </div>
+        <div className="second" style={Style2}>
+          <h2>SECOND DISPLAY </h2>
+        </div>
       </div>
       );
   }
