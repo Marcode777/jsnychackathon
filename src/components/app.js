b2845904d66a50c655124853e31e30cc8e40a335
Marcode777 <marcosebello7@gmail.com>
2016-12-27 11:13:32 -0500

    created display.js file exported as component, tested component, works

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Display from './display';
 
 export default class App extends React.Component{
   render(){
     return(
       <div>
         <h2>jsnyc hackathon</h2>
+        <Display/>
       </div>
       );
   }
---
+++ src/components/display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Display extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2>DISPLAY FILE </h2>
+      </div>
+      );
+  }
+}
