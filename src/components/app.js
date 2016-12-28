ffecf4e61caa260b0c64de3b1fc5a30b812fa448
Marcode777 <marcosebello7@gmail.com>
2016-12-28 15:34:51 -0500

    made some more changes, in progress

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,6 @@ export default class App extends React.Component{
   render(){
     return(
       <div>
-        <h2>jsnyc hackathon</h2>
         <Display/>
       </div>
       );
--- src/components/display.js
+++ src/components/display.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Style1 = {
-  backgroundImage: 'url("https://static.pexels.com/photos/2324/skyline-buildings-new-york-skyscrapers.jpg")',
-  // position:"absolute",
+  backgroundImage: 'url("http://cdn.wonderfulengineering.com/wp-content/uploads/2014/04/code-wallpaper-19.png")',
+  // position:"fized",
   WebkitBackgroundSize: 'cover', // note the capital 'W' here
   height:"100%",
   width:"100%",
   backgroundColor: "white",
-  minHeight: 500,
+  minHeight: 600,
 }
 
 const Style2 = {
@@ -16,16 +16,24 @@ const Style2 = {
 }
 
 
+
 export default class Display extends React.Component{
   render(){
     return(
-      <div className="container">
-        <div className="first" style={Style1}>
-          <h2>FIRST DISPLAY </h2>
-        </div>
-        <div className="second" style={Style2}>
-          <h2>SECOND DISPLAY </h2>
-        </div>
+      <div>
+
+        
+
+          <div className="first" style={Style1}>
+            <h1 style={{color:'white'}}>JSNYC --- HACKATHON</h1>
+            <h3></h3>
+          </div>
+          <div className="second" style={Style2}>
+            <h2>SECOND DISPLAY </h2>
+          </div>
+
+
+
       </div>
       );
   }
