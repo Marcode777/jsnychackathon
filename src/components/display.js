722d041ecd80b68f0a8c6fec2aece75410292f20
Marcode777 <marcosebello7@gmail.com>
2016-12-30 17:19:15 -0500

    tried to implement grid system on second component to second div, but it is not implementing for some reason and a solution still needs to be found on how to make it  function properly

--- src/components/display.js
+++ src/components/display.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {Component} from 'react';
 import './app.css';
+import ReactDOM from 'react-dom';
+import Container from 'muicss/lib/react/container';
+import Row from 'muicss/lib/react/row';
+import Col from 'muicss/lib/react/col';
+
 
 const Style1 = {
   backgroundImage: 'url("http://4.bp.blogspot.com/-v95TY0bAoVc/VfEHTPbnyOI/AAAAAAAA4ck/cg6Ra0creME/s1600/15660.jpg")',
@@ -37,9 +42,17 @@ export default class Display extends React.Component{
             <h2 className="secondh2">JSNYC hackathon</h2>
             <h2 className="thirdh2">03/07/17</h2>
           </div>
-           <div className="second" style={Style2}> 
-            <h3>SECOND DISPLAY </h3>
-           </div> 
+
+          <div className="second" style={Style2}> 
+            <Container fluid={true}>
+              <Row>
+                <Col md="4">md-4</Col>
+                <Col md="4">md-4</Col>
+                <Col md="4">md-4</Col>
+              </Row>
+            </Container> 
+            <h3>SECOND DISPLAY, THIS SECTION SHOULD HAVE A GRID SYSTEM WITH MORE INFO ABOUT THE EVENT </h3>
+          </div> 
 
 
 
